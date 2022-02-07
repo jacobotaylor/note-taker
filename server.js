b21b7ed811e99b8303b4f925a4887c05482e26d2
jacobotaylor <93738407+jacobotaylor@users.noreply.github.com>
2022-02-06 19:51:22 -0500

    have to clean up wonky paths, start to the majority of the project

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+app.get('api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
+// GET Route for feedback page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
